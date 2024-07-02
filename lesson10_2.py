4fbe0eb958c42ce7083afb3679e5baedcf25439e
葉鈺希 <140705895+Ye-Yu-Xi@users.noreply.github.com>
2024-07-02 09:43:53 +0000

    modify

--- lesson10_2.py
+++ lesson10_2.py
@@ -1,11 +1,13 @@
 import random
-names_list = []
-with open("names.txt",encoding="utf-8") as file:  #同上上的程式區塊結果 只是方法不一樣，方法2
-    for line in file:
-        names_list.append(line[:3])
-        
+
+def allNames() ->list:
+    names_list = []
+    with open("names.txt",encoding="utf-8") as file:
+        for line in file:
+            names_list.append(line[:3])
+            
 first_name = input("請輸入您的姓:")
 count = int(input("請輸入您要的筆數:"))
 random_names = random.choices(names_list,k=count)    #K=要抓幾個(預設是一個)
 for name in random_names:
-    print(first_name + name[-2:])
+    print(first_name + name[-2:])     #2:13:00
